32e22f0b5ba5b117dc055df8ab12f3b25846ad8c
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2024-10-24 01:57:42 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#define MOD 10007
+using namespace std;
+
+int dp[1001];
+int N;
+void Input()
+{
+    cin >> N;
+    fill_n(dp, 1001, -1);
+}
+int main()
+{
+    Input();
+    
+    dp[1] = 1;
+    dp[2] = 3;
+    
+    for(int i = 3; i<= N; ++i)
+    {
+        dp[i] = (dp[i-1]+ dp[i-2]*2) % MOD;
+    }
+    
+    cout << dp[N];
+    return 0;
+}
