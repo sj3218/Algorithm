8a604741ab44cc43d06f4d7004f72581271de485
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2024-10-23 20:27:57 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+long long arr[100];
+int n;
+
+int main()
+{
+    cin >> n;
+    arr[0] = 0;
+    arr[1] = 1;
+    
+    for(int i = 2; i <=n; ++i)
+    {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    cout << arr[n];
+    return 0;
+}
