175983a46446055365d0d0c79dcb97c409d2b7be
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2024-11-01 03:26:41 +0900

    [Silver V] Title: 귀찮음, Time: 0 ms, Memory: 2020 KB, Score: 18 point -BaekjoonHub

---
+++ 백준/Silver/16208. 귀찮음/귀찮음.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr;
+    int sum = 0;
+    int val;
+    for(int i = 0; i< n;++i)
+    {
+        cin >> val;
+        sum += val;
+        arr.push_back(val);
+    }
+    
+    long long ans = 0;
+    for(int i = 0; i< n; ++i)
+    {
+        sum -= arr[i];
+        ans += sum * arr[i];
+    }
+    
+    cout << ans;
+    return 0;
+}
