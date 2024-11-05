d4e805b52d546e990e754acba881541d381697db
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2024-11-05 18:11:00 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,50 +1,40 @@
 #include <iostream>
-#include <cstring>
+#include <algorithm>
 
 using namespace std;
-
+int N;
 int dp[1000001];
 
 int main()
 {
-    int N;
     cin >> N;
-    memset(dp, -1, sizeof(dp));
-
-    dp[1] = 0;
-    dp[2] = 1;
-    dp[3] = 1;
-
-    for (int i = 4; i <= N; ++i)
+    
+    fill_n(dp, 1000001, 0);
+    
+    for(int i = 2; i<= N; ++i)
     {
-        if (i % 3 == 0)
+        if(i%3 == 0)
         {
-            dp[i] = dp[i / 3] + 1;
-        }
-
-        if (i % 2 == 0)
-        {
-            if (dp[i] != -1)
+            if(i% 2 == 0)
             {
-                dp[i] = min(dp[i], dp[i / 2] + 1);
+                dp[i] = min(dp[i/3], dp[i/2]);
+                dp[i] = min(dp[i], dp[i-1]) + 1;
             }
             else
             {
-                dp[i] = dp[i / 2] + 1;
+                dp[i] = min(dp[i/3], dp[i-1]) + 1;
             }
         }
-
-        if (dp[i] == -1)
+        else if(i% 2 ==0)
         {
-            dp[i] = dp[i - 1] + 1;
+            dp[i] = min(dp[i/2], dp[i-1]) + 1;
         }
         else
         {
-            dp[i] = min(dp[i], dp[i - 1] + 1);
+            dp[i] = dp[i-1] + 1;
         }
-
     }
-
+    
     cout << dp[N];
     return 0;
 }
