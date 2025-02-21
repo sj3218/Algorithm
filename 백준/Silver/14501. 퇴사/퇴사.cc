eb5bf2aaef3a1092e90fcc1e6994a080cc71c595
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-02-22 05:25:12 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N;
+int arr[16][2];
+int dp[16];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> N;
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> arr[i][0] >> arr[i][1];
+    }
+
+
+    for (int i = N - 1; i >= 0; --i)
+    {
+        if (i + arr[i][0] > N)
+        {
+            dp[i] = dp[i + 1];
+            continue;
+        }
+        dp[i] = max(dp[i + 1], dp[i + arr[i][0]] + arr[i][1]);
+    }
+
+    cout << dp[0];
+    return 0;
+}
