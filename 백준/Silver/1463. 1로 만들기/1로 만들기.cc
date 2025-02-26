11857f0b941427225da71ff6447c53f4922cdf6d
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-02-26 16:18:50 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,40 +1,37 @@
 #include <iostream>
-#include <algorithm>
-
-using namespace std;
-int N;
+#define INF 0X7FFFFFFF
 int dp[1000001];
+int N;
+using namespace std;
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
     cin >> N;
-    
-    fill_n(dp, 1000001, 0);
-    
-    for(int i = 2; i<= N; ++i)
+
+    dp[1] = 0;
+    dp[2] = 1;
+    dp[3] = 1;
+
+    for (int i = 4; i <= N; ++i)
     {
-        if(i%3 == 0)
+        int ans = INF;
+        if (i % 3 == 0)
         {
-            if(i% 2 == 0)
-            {
-                dp[i] = min(dp[i/3], dp[i/2]);
-                dp[i] = min(dp[i], dp[i-1]) + 1;
-            }
-            else
-            {
-                dp[i] = min(dp[i/3], dp[i-1]) + 1;
-            }
+            ans = min(ans, dp[i / 3] + 1);
         }
-        else if(i% 2 ==0)
+
+        if (i % 2 == 0)
         {
-            dp[i] = min(dp[i/2], dp[i-1]) + 1;
+            ans = min(ans, dp[i / 2] + 1);
         }
-        else
-        {
-            dp[i] = dp[i-1] + 1;
-        }
+
+        ans = min(ans, dp[i - 1] + 1);
+        dp[i] = ans;
     }
-    
+
     cout << dp[N];
     return 0;
 }
