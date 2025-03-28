f8b7c6c2aa0e51d98b322cde1eca753100a659c7
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-03-28 16:45:41 +0900

    [Gold III] Title: 불!, Time: 48 ms, Memory: 10964 KB -BaekjoonHub

---
+++ 백준/Gold/4179. 불！/불！.cc
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int R, C;
+char map[1001][1001];
+int pos_J_r, pos_J_c;
+int is_visited[1001][1001];
+int fire_visited[1001][1001];
+queue<pair<int, int>> q_fire;
+
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1, 0, 0 };
+
+void Input()
+{
+    fill_n(is_visited[0], 1001 * 1001, -1);
+    fill_n(fire_visited[0], 1001 * 1001, -1);
+
+    cin >> R >> C;
+    for (int i = 0; i < R; ++i)
+    {
+        for (int j = 0; j < C; ++j)
+        {
+            cin >> map[i][j];
+            if (map[i][j] == 'J')
+            {
+                pos_J_r = i;
+                pos_J_c = j;
+            }
+            else if (map[i][j] == 'F')
+            {
+                q_fire.push({ i, j });
+                fire_visited[i][j] = 0;
+            }
+        }
+    }
+}
+
+bool IsValid(int x, int y)
+{
+    if (x < 0 || y < 0 || x >= R || y >= C)
+        return false;
+    return true;
+}
+
+void FireBFS()
+{
+    while (!q_fire.empty())
+    {
+        int x = q_fire.front().first;
+        int y = q_fire.front().second;
+        q_fire.pop();
+
+        int curr_fire = fire_visited[x][y];
+
+        for (int i = 0; i < 4; ++i)
+        {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (!IsValid(nx, ny) || fire_visited[nx][ny] != -1)
+                continue;
+
+            if (map[nx][ny] == '#')
+                continue;
+
+            q_fire.push({ nx, ny });
+            fire_visited[nx][ny] = curr_fire + 1;
+        }
+    }
+}
+
+int bfs()
+{
+    queue<pair<int, int>> q;
+    q.push({ pos_J_r, pos_J_c });
+    is_visited[pos_J_r][pos_J_c] = 0;
+
+    while (!q.empty())
+    {
+        int x = q.front().first;
+        int y = q.front().second;
+        int curr_time = is_visited[x][y];
+        q.pop();
+
+        if (x == 0 || x == R - 1 || y == 0 || y == C - 1)
+        {
+            return is_visited[x][y] + 1;
+        }
+
+        for (int i = 0; i < 4; ++i)
+        {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (!IsValid(nx, ny) || is_visited[nx][ny] != -1)
+            {
+                continue;
+            }
+
+            if (fire_visited[nx][ny] > curr_time + 1 || fire_visited[nx][ny] == -1)
+            {
+                if (map[nx][ny] == '.')
+                {
+                    q.push({ nx, ny });
+                    is_visited[nx][ny] = curr_time + 1;
+                }
+            }
+
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    Input();
+    FireBFS();
+    int answer = bfs();
+
+    if (answer == -1)
+        cout << "IMPOSSIBLE";
+    else
+        cout << answer;
+    return 0;
+}
