75541d6bdc4d8b0a0e12fd052d2157233eec9272
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-04-15 04:32:47 +0900

    [Bronze I] Title: 단어 공부, Time: 8 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.cc
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -1,33 +1,69 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
+#define ALPHABET 26
 
 using namespace std;
 
-int alpha[26], cnt =0;
-string input;
-//아스키코드 : 대문자 65~90, 소문자 97~122
+string s;
+int dp[ALPHABET];
 
-int main() {
-    cin >> input;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> s;
 
-    //빈도수 체킹
-    for(int i=0;i<input.length();i++) {
-        if(input[i]<97) alpha[input[i] - 65]++; //대문자
-        else alpha[input[i] - 97]++; //소문자
+    int size = s.size();
+    int idx = 0;
+
+    for (int i = 0; i < size; ++i)
+    {
+        char c = s[i];
+
+        if (c >= 'A' && c <= 'Z')
+        {
+            idx = c - 'A';
+        }
+        else if (c >= 'a' && c <= 'z')
+        {
+            idx = c - 'a';
+        }
+        else
+        {
+            continue;
+        }
+
+        ++dp[idx];
     }
 
-    int max = 0, max_indx=0;
+    int answer = 0;
+    bool is_existed = false;
 
-    for(int i=0;i<26;i++) {
-        if(max<alpha[i]) {
-            max=alpha[i];
-            max_indx = i;
+    for (int i = 0; i < ALPHABET; ++i)
+    {
+        if (answer == dp[i])
+        {
+            if (answer == 0)
+                continue;
+            is_existed = true;
+            continue;
+        }
+        else if (answer < dp[i])
+        {
+            is_existed = false;
+            answer = dp[i];
+            idx = i;
         }
     }
-    //가장 많이 사용된 알파벳이 여러개인가?
-    for(int i=0;i<26;i++) {
-        if(max==alpha[i]) cnt++;
+
+    if (is_existed)
+        cout << "?";
+    else
+    {
+        char c = 'A' + idx;
+        cout << c;
     }
-    //여러개라면 ? 출력, 아니라면 대문자로 출력
-    if(cnt>1) cout << "?";
-    else cout << (char)(max_indx+65);
+
+    return 0;
 }
