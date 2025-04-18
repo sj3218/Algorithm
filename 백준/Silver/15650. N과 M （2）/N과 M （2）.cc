c9658ef80e1a7c4f7230d5fab2aa43b3afa44f18
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-04-18 17:48:22 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,51 +1,44 @@
 #include <iostream>
 #include <algorithm>
-#define MAX 9
+#include <vector>
 
 using namespace std;
-
 int N, M;
-int arr[MAX];
-int is_visited[MAX];
 
-void Input()
-{
-    cin >> N >> M;
-    fill_n(arr, MAX, 0);
-    fill_n(is_visited, MAX, false);
-}
+int is_visited[10];
+int answer[10];
 
-void Recursive(int k,int val)
+void recursive(int idx, int start)
 {
-    if (k == M)
+    if (idx == M)
     {
         for (int i = 0; i < M; ++i)
         {
-            cout << arr[i] << " ";
+            cout << answer[i] << " ";
         }
         cout << "\n";
-
         return;
     }
 
-    for (int i = val; i <= N; ++i)
+    for (int i = start; i <= N; ++i)
     {
-        if (is_visited[i] == true)
-        {
+        if (is_visited[i])
             continue;
-        }
 
-        arr[k] = i;
         is_visited[i] = true;
-        Recursive(k+1, i+1);
+        answer[idx] = i;
+        recursive(idx + 1, i+1);
         is_visited[i] = false;
     }
 }
 
 int main()
 {
-    Input();
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
 
-    Recursive(0,1);
+    cin >> N >> M;
+    recursive(0,1);
+
     return 0;
 }
