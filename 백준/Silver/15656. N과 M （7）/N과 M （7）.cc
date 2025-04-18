cb1adbde8237033301fe977fdd64fbe41e19aa47
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-04-18 22:08:57 +0900

    [Silver III] Title: N과 M (7), Time: 396 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int N, M;
+int arr[10];
+
+int is_visited[10];
+int answer[10];
+
+void recursive(int idx)
+{
+    if (idx == M)
+    {
+        for (int i = 0; i < M; ++i)
+        {
+            cout << answer[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 0; i < N; ++i)
+    {
+        
+        answer[idx] = arr[i];
+        recursive(idx + 1);
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> N >> M;
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, arr + N);
+
+    recursive(0);
+
+    return 0;
+}
