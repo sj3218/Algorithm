04858a52fda2a5d431b93340cf4ae521982d80d3
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-04-21 20:32:31 +0900

    [Silver II] Title: 연속합, Time: 4 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,42 +1,32 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
-
-int dp[100001];
 int N;
 int arr[100001];
-
-void Input()
-{
-    cin >> N;
-    fill_n(arr, 100001, -1);
-    fill_n(dp, 100001, -1);
-    
-    for(int i = 0; i< N; ++i)
-    {
-        cin >>arr[i];
-    }
-}
-
+int dp[100001];
 
 int main()
 {
-    Input();
-    int M = 0;
-    
-    dp[0] = arr[0];
-    M = dp[0];
-    
-    for(int i = 1; i<N; ++i)
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> N;
+    for (int i = 1; i <= N; ++i)
     {
-        dp[i] = max(arr[i], dp[i-1]+arr[i]);
-        if(M < dp[i])
-        {
-            M = dp[i];
-        }
+        cin >> arr[i];
     }
-    
-    cout<<M;
+
+    dp[1] = arr[1];
+    int answer = dp[1];
+
+    for (int i = 2; i <= N; ++i)
+    {
+        dp[i] = max(arr[i], dp[i - 1] + arr[i]);
+        answer = max(answer, dp[i]);
+    }
+
+    cout << answer;
     return 0;
 }
