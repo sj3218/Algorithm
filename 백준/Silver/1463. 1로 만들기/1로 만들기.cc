ebdadc0ed2886314021752c3a96aaddf45f71067
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-04-21 14:29:11 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,8 +1,11 @@
 #include <iostream>
-#define INF 0X7FFFFFFF
-int dp[1000001];
-int N;
+#include <algorithm>
+#include <vector>
+
 using namespace std;
+
+int N;
+int dp[1000001];
 
 int main()
 {
@@ -10,28 +13,18 @@ int main()
     cin.tie(0); cout.tie(0);
 
     cin >> N;
-
     dp[1] = 0;
-    dp[2] = 1;
-    dp[3] = 1;
 
-    for (int i = 4; i <= N; ++i)
+    for (int i = 2; i <= N; ++i)
     {
-        int ans = INF;
-        if (i % 3 == 0)
-        {
-            ans = min(ans, dp[i / 3] + 1);
-        }
-
+        dp[i] = dp[i - 1] + 1;
         if (i % 2 == 0)
-        {
-            ans = min(ans, dp[i / 2] + 1);
-        }
-
-        ans = min(ans, dp[i - 1] + 1);
-        dp[i] = ans;
+            dp[i] = min(dp[i], dp[i / 2] + 1);
+        if (i % 3 == 0)
+            dp[i] = min(dp[i], dp[i / 3] + 1);
     }
 
     cout << dp[N];
+
     return 0;
 }
