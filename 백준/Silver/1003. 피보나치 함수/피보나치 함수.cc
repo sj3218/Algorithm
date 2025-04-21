d7a3820a9dd2ba433dcb680570ccf84f298706fa
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-04-21 18:44:09 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -1,52 +1,36 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+
 using namespace std;
 
 int T, N;
-int dp[41][2];
-
-void dfs(int n)
-{
-    if(n < 0)
-        return;
-    
-    if(dp[n][0] != -1)
-    {
-        return;
-    }
-    
-    if(dp[n-1][0] == -1)
-    {
-        dfs(n-1); 
-    }
-    if(dp[n-2][0] == -1)
-    {
-        dfs(n-2);
-    }
-    
-    dp[n][0] = dp[n-1][0] + dp[n-2][0];
-    dp[n][1] = dp[n-1][1] + dp[n-2][1];
-}
+int dp_zero[41];
+int dp_one[41];
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    
-    fill_n(dp[0], 41*2, -1);
-    dp[0][0] = 1;
-    dp[0][1] = 0;
-    dp[1][0] = 0;
-    dp[1][1] = 1;
-    
+
+    dp_zero[0] = 1;
+    dp_zero[1] = 0;
+    dp_one[0] = 0;
+    dp_one[1] = 1;
+
+    for (int i = 2; i <= 40; ++i)
+    {
+        dp_one[i] = dp_one[i - 1] + dp_one[i - 2];
+        dp_zero[i] = dp_zero[i - 1] + dp_zero[i - 2];
+    }
+
     cin >> T;
-    for(int i = 0; i< T; ++i)
+
+    while (T-- > 0)
     {
         cin >> N;
-        
-        dfs(N);
-        cout << dp[N][0] <<" "<<dp[N][1]<<"\n";
-        
+        cout << dp_zero[N] << " " << dp_one[N] <<"\n";
     }
+
     return 0;
 }
