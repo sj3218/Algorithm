9d2a3a5c6401c96a256d8fb393181d330d5645c7
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-04-23 14:36:46 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int N;
 int arr[1001];
 int dp[1001];
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-
-    fill_n(dp, 1001, 0);
 
     cin >> N;
     for (int i = 1; i <= N; ++i)
@@ -20,21 +20,18 @@ int main()
         dp[i] = arr[i];
     }
 
-    int val;
-    for (int i = 1; i <= N; ++i)
+    for (int i = 2; i <= N; ++i)
     {
-        val = arr[i];
-
+        int val = arr[i];
         for (int j = 1; j < i; ++j)
         {
             if (val > arr[j])
             {
-                dp[i] = max(dp[i], dp[j] + arr[i]);
+                dp[i] = max(dp[i], dp[j] + val);
             }
         }
     }
 
-    cout << *max_element(dp, dp + N + 1);
-
+    cout << *max_element(dp+1, dp + N+1);
     return 0;
 }
