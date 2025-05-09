05ec11686a6f1327aaa6b7aa018287fa207b4263
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-05-10 02:45:14 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 36 ms, Memory: 4752 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#define INF 0X7FFFFFFF
+
+using namespace std;
+
+int N, M;
+int start_city, end_city;
+vector<vector<pair<int, int>>> graph(1001);
+int dist[1001];
+int pre_city[1001];
+
+void dijkstra()
+{
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({ 0, start_city });
+    dist[start_city] = 0;
+
+    int curr_city, curr_w;
+    while (!pq.empty())
+    {
+        curr_w = pq.top().first;
+        curr_city = pq.top().second;
+        pq.pop();
+
+        if (dist[curr_city] < curr_w)
+            continue;
+
+        int nx_city, nx_w;
+        for (pair<int, int> next : graph[curr_city])
+        {
+            nx_city = next.first;
+            nx_w = next.second;
+
+            if (dist[nx_city] <= nx_w + dist[curr_city])
+                continue;
+
+            dist[nx_city] = nx_w + dist[curr_city];
+            pq.push({ dist[nx_city], nx_city });
+            pre_city[nx_city] = curr_city;
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> N >> M;
+
+    int s, e, w;
+    for (int i = 0; i < M; ++i)
+    {
+        cin >> s >> e >> w;
+        graph[s].push_back({ e,w });
+    }
+
+    cin >> start_city >> end_city;
+
+    fill_n(dist, 1001, INF);
+    dijkstra();
+
+    vector<int> route;
+    int idx = end_city;
+
+    while (idx != start_city)
+    {
+        route.push_back(idx);
+        idx = pre_city[idx];
+    }
+    route.push_back(start_city);
+
+    cout << dist[end_city] << "\n";
+    cout << route.size() << "\n";
+    for (int i = (int)route.size() - 1; i >= 0; --i)
+    {
+        cout << route[i] << " ";
+    }
+
+    return 0;
+}
