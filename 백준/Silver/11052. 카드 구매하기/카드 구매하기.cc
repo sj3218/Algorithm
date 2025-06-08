d16a0567f90e39b23b5ccff1fcd97001f3265581
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-06-08 23:55:14 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int d[1001];
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n;cin>>n;
+  for(int i=1;i<=n;++i) cin>>d[i];
+  for(int i=1;i<=n;++i) for(int j=1;j<i;++j) d[i] = max(d[i-j] + d[j], d[i]);
+  cout << d[n];
+}
