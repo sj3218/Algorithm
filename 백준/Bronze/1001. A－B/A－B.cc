e073aada990aadf2c688d1e873b47c36849235d7
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-07-04 00:00:57 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <tuple>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int a, b;
+    cin >> a>>b;
+    cout << a-b;
+    return 0;
+}
