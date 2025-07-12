acdbc48a836652fa65d280386177b1f752069468
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-07-13 00:28:20 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 28 ms, Memory: 3004 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -1,37 +1,45 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
-using namespace std;
-#define INF 1000000000
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
-int N, r, c;
-int sum[501], matrix[501][2], dp[501][501];
+using namespace std;
+#define INF 0X7FFFFFFF
+
+int N;
+pair<int, int> mat[501];
+int dp[501][501];
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    
+    fill_n(&dp[0][0], 501 * 501, INF);
     cin >> N;
-
-    for (int i = 1; i <= N; i++)
+    int r, c;
+    for (int i = 1; i <= N; ++i)
     {
-	cin >> r >> c;
-	matrix[i][0] = r;
-	matrix[i][1] = c;
+        cin >> r >> c;
+        mat[i] = { r,c };
+        dp[i][i] = 0;
     }
 
-    for (int i = 1; i < N; i++)
+    /*
+    dp[i][j] = dp[i][k] + dp[k+ 1][j] + mat[i].r * mat[k].c * mat[j].c;
+    */
+    int j = 0;
+    for (int len = 1; len < N; ++len)
     {
-	for (int j = 1; i + j <= N; j++)
-	{
-	    dp[j][i + j] = INF;
-	    for (int k = j; k <= i + j; k++)
-	    {
-		dp[j][i + j] = min(dp[j][i + j], dp[j][k] + dp[k + 1][i + j] + matrix[j][0] * matrix[k][1] * matrix[i + j][1]);
-	    }
-	}
-
+        for (int i = 1; i + len <= N; ++i)
+        {
+            j = i + len;
+            for (int k = i; k < j; ++k)
+            {
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] + mat[i].first * mat[k].second * mat[j].second);
+            }
+        }
     }
 
     cout << dp[1][N];
-
-
+    return 0;
 }
