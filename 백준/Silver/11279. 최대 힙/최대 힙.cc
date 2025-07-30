3e7baa4f235d2fdb63c99309cdc7bb98f7a66f5a
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-07-30 14:35:18 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int N;
+priority_queue<int> pq;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> N;
+
+    int x;
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> x;
+        if (x == 0)
+        {
+            if (pq.empty())
+                cout << "0\n";
+            else
+            {
+                cout<< pq.top()<<"\n";
+                pq.pop();
+            }
+        }
+        else
+        {
+            pq.push(x);
+        }
+    }
+
+    return 0;
+}
