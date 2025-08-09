ec730e970de76aa962b3540c13120d269213004a
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-08-09 18:15:15 +0900

    [Silver IV] Title: 수 찾기, Time: 44 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,51 +1,50 @@
 #include <iostream>
 #include <algorithm>
+#include <string>
+#include <queue>
+#include <tuple>
+#include <vector>
+#include <map>
 
 using namespace std;
+#define INF 0X7FFFFFFF
+
 int N, M;
+int arr1[100001];
 
-int arr[100001];
-
-int binarysearch(int a)
-{
-    int start = 0;
-    int end = N-1;
-    int mid;
-    while(start <= end)
-    {
-        mid = (start+end)/2;
-        if(arr[mid] < a)
-        {
-            start = mid+1;
-        }
-        else if(arr[mid] > a)
-        {
-            end = mid-1;
-        }
-        else
-            return 1;
-    }
-    return 0;
-}
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0); 
-    
+    cin.tie(0); cout.tie(0);
+
     cin >> N;
-    for(int i = 0; i< N; ++i)
+    for (int i = 0; i < N; ++i)
     {
-        cin >> arr[i];
+        cin >> arr1[i];
     }
-    sort(arr, arr+N);
     
-    cin >> M;
+    sort(arr1, arr1 + N);
     int num;
-    for(int j = 0; j< M; ++j)
+    int idx;
+
+    cin >> M;
+    for (int j = 0; j < M; ++j)
     {
         cin >> num;
-        cout << binarysearch(num) <<"\n";
+
+        idx = lower_bound(arr1, arr1 + N, num) - arr1;
+        if (idx >= N)
+        {
+            cout << "0\n";
+            continue;
+        }
+
+        if(arr1[idx] != num)
+            cout << "0\n";
+        else
+            cout << "1\n";
+
     }
-    
+
     return 0;
 }
