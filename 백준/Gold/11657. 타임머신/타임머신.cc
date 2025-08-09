3392f7980ff9e503058749f587a7992f798633ba
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-08-09 23:58:53 +0900

    [Gold IV] Title: 타임머신, Time: 12 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.cc
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -8,66 +8,58 @@
 using namespace std;
 #define INF 0X7FFFFFFF
 
-int city_cnt;
-int bus_cnt;
-vector<tuple<int, int, int>> bus_info;
+vector<pair<int, int>> bus_info[501];
+int city_cnt, bus_cnt;
+int a, b, c;
+
 long long cost[501];
-
-bool bellman_ford(int start)
-{
-    cost[start] = 0;
-    int a, b, c;
-    for (int i = 1; i <= city_cnt - 1; ++i)
-    {
-        for (tuple<int, int, int> info : bus_info)
-        {
-            tie(a, b, c) = info;
-            if (cost[a] == INF)
-                continue;
-            if (cost[b] <= cost[a] + c)
-                continue;
-
-            cost[b] = cost[a] + c;
-        }
-    }
-
-    for (tuple<int, int, int> info : bus_info)
-    {
-        tie(a, b, c) = info;
-        if (cost[a] != INF && cost[b] > cost[a] + c)
-            return false;
-    }
-
-    return true;
-}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
 
+    fill_n(cost, 501, INF);
     cin >> city_cnt >> bus_cnt;
-
-    int A, B, C;
     for (int i = 0; i < bus_cnt; ++i)
     {
-        cin >> A >> B >> C;
-        bus_info.push_back({ A,B,C });
+        cin >> a >> b >> c;
+        bus_info[a].push_back({ b,c });
     }
 
-    fill_n(cost, 501, INF);
-    if (bellman_ford(1))
+    cost[1] = 0;
+    for (int i = 0; i < city_cnt; ++i)
     {
-        for (int i = 2; i <= city_cnt; ++i)
+        for (int j = 1; j <= city_cnt; ++j)
         {
-            if (cost[i] == INF)
-                cout << "-1\n";
-            else
-                cout << cost[i] << "\n";
+            for (pair<int, int> bus : bus_info[j])
+            {
+                int next_pos = bus.first;
+                int c = bus.second;
+
+                if (cost[j] == INF)
+                    continue;
+                if (cost[next_pos] <= cost[j] + c)
+                    continue;
+
+                cost[next_pos] = cost[j] + c;
+
+                if (i == city_cnt - 1)
+                {
+                    cout << -1;
+                    return 0;
+                }
+            }
         }
     }
-    else
-        cout << -1;
+
+    for (int i = 2; i <= city_cnt; ++i)
+    {
+        if (cost[i] == INF)
+            cout << "-1\n";
+        else
+            cout << cost[i] <<"\n";
+    }
 
     return 0;
 }
