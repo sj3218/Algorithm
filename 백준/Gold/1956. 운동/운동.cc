2a60808366453905eb9430174ce01c3fb1a1242d
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-08-13 23:09:28 +0900

    [Gold IV] Title: 운동, Time: 152 ms, Memory: 4496 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+#define INF 0X7FFFFFFF
+
+int V, E;
+vector<vector<pair<int, int>>> graph(401);
+int cost[401][401];
+bool is_visited[401];
+int answer = INF;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> V >> E;
+    int a, b, c;
+
+    fill_n(&cost[0][0], 401 * 401, INF);
+    for (int i = 0; i < E; ++i)
+    {
+        cin >> a >> b >> c;
+        graph[a].push_back({ b,c });
+        cost[a][b] = c;
+    }
+
+    for (int k = 1; k <= V; ++k)//경유지
+    {
+        for (int i = 1; i <= V; ++i)//start
+        {
+            for (int j = 1; j <= V; ++j)//end
+            {
+                if (i == j)
+                {
+                    cost[i][j] = 0;
+                    continue;
+                }
+
+                if (cost[i][k] != INF && cost[k][j] != INF)
+                {
+                    cost[i][j] = min(cost[i][j], cost[i][k] + cost[k][j]);
+                }
+            }
+        }
+    }
+
+    for (int i = 1; i <= V; ++i)
+    {
+        for (int j = 1; j <= V; ++j)
+        {
+            if (i == j)
+                continue;
+
+            if (cost[i][j] == INF || cost[j][i] == INF)
+                continue;
+            answer = min(answer, cost[i][j] + cost[j][i]);
+        }
+    }
+
+    if (answer == INF)
+        cout << -1;
+    else
+        cout << answer;
+    return 0;
+}
