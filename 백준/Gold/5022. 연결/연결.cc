246df315d4da0a1a0490367f710b4e2f58672471
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2025-09-25 17:16:57 +0900

    [Gold I] Title: 연결, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/5022. 연결/연결.cc
+++ 백준/Gold/5022. 연결/연결.cc
@@ -1,438 +1,172 @@
-
-#include<iostream>
-
-#include<queue>
-
-#include<vector>
-
-#include<cstring>
-
- 
-
-#define endl "\n"
-
-#define MAX 101
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <tuple>
+#include <vector>
 
 using namespace std;
+#define INF 0X7FFFFFFF
 
- 
-
-typedef struct
-
+struct track
 {
-
     int x;
-
     int y;
+    int dist;
+};
 
-    vector<pair<int, int>> Route;
+int N, M;
+vector<pair<int, int>> points;
+track is_visited[101][101];
+bool has_line[101][101];
 
-}Info;
-
- 
-
-int N, M, Answer = 999999999;
-
-int A_Length, B_Length;
-
-int MAP[MAX][MAX];
-
-bool Temp_Visit[MAX][MAX];
-
-int Visit[MAX][MAX];
-
-bool A_Flag, B_Flag, Answer_Flag;
-
- 
-
-pair<int, int> A_Pos[2], B_Pos[2];
-
- 
-
-int dx[] = { 0, 0, 1, -1 };
-
-int dy[] = { 1, -1, 0, 0 };
-
- 
-
-int Min(int A, int B) { if (A < B) return A; return B; }
-
- 
-
-void Print()
-
-{
-
-    cout << "###################################################" << endl;
-
-    for (int i = 0; i <= N; i++)
-
-    {
-
-        for (int j = 0; j <= M; j++)
-
-        {
-
-            cout << Visit[i][j] << " ";
-
-        }
-
-        cout << endl;
-
-    }
-
-    cout << "###################################################" << endl;
-
-}
-
- 
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1, 0,0 };
 
 void Input()
-
 {
-
-    cin >> M >> N;
-
- 
-
-    for (int i = 0; i < 2; i++)
-
+    cin >> N >> M;
+    int a, b;
+    for (int i = 0; i < 4; ++i)
     {
-
-        int x, y; cin >> x >> y;
-
-        A_Pos[i].first = y;
-
-        A_Pos[i].second = x;
-
+        cin >> a >> b;
+        points.push_back({ a,b });
     }
-
-    for (int i = 0; i < 2; i++)
-
-    {
-
-        int x, y; cin >> x >> y;
-
-        B_Pos[i].first = y;
-
-        B_Pos[i].second = x;
-
-    }
-
 }
 
- 
-
-void BFS(int a, int b, int da, int db, char c)
-
-// BFS(시작 x좌표, 시작 y좌표, 도착 x좌표, 도착 y좌표, A를 까는지 or B를 까는지 구분)
-
+bool OutOfBound(int x, int y)
 {
+    if (x <0 || y < 0 || x>N || y > M)
+        return true;
+    return false;
+}
 
-    queue<Info> Q;
+void MakeLine(pair<int,int> goal)
+{
+    int cx = goal.first;
+    int cy = goal.second;
+    int temp;
 
-    // 위에서 말한대로 구조체를 자료형으로 사용하는 Queue 선언.
+    fill_n(&has_line[0][0], 101 * 101, false);
 
-    vector<pair<int, int>> V;
-
-    Info Temp;
-
-    Temp.x = a; Temp.y = b; Temp.Route.push_back(make_pair(a, b));
-
-    Q.push(Temp);
-
-    // Queue에 초기값 Push
-
-    Temp_Visit[a][b] = true;
-
-    if (c == 'A') Visit[a][b] = 1;
-
-    // A전선을 깔면 Visit배열을 1로 표현
-
-    else Visit[a][b] = 2;
-
-    // B전선을 깔면 Visit배열을 2로 표현
-
-    // => 즉, Visit배열이 0이다 = 아무 전선도 깔려 있지 않은 곳이다 를 의미.
-
- 
-
-    while (Q.empty() == 0)
-
+    while (cx != -1 && cy != -1)
     {
+        has_line[cx][cy] = true;
+        temp = is_visited[cx][cy].x;
+        cy = is_visited[cx][cy].y;
+        cx = temp;
+    }
+}
 
-        int x = Q.front().x;
+bool bfs(pair<int, int> start, pair<int, int> goal, bool first_check)
+{
+    queue<pair<int,int>> q;
+    int cx, cy;
+    int nx, ny;
+    int length;
+    cx = start.first;
+    cy = start.second;
+    q.push({ cx,cy });
+    is_visited[cx][cy] = { -1,-1, 0 };
 
-        int y = Q.front().y;
-
-        vector<pair<int, int>> R = Q.front().Route;
-
-        Q.pop();
-
- 
-
-        if (x == da && y == db)    // 도착점에 도착한다면
-
+    while (!q.empty())
+    {
+        tie(cx, cy) = q.front(); q.pop();
+        length = is_visited[cx][cy].dist;
+        
+        if (cx == goal.first && cy == goal.second)
         {
+            if (first_check)
+                MakeLine(goal);
 
-            if (c == 'A')        // A전선을 까는 중이였다면
-
-            {
-
-                A_Flag = true;    // A를 깔았다고 표시.
-
-                for (int i = 0; i < R.size(); i++)
-
-                {
-
-                    int Rx = R[i].first;
-
-                    int Ry = R[i].second;
-
-                    Visit[Rx][Ry] = 1;
-
- 
-
-                    A_Length++;
-
-                }
-
-                // 위의 for문은 연결된 최단경로에 이미 전선을 깔았다고
-
-                // 표시해주는 역할. 동시에 길이를 계산함.
-
-            }
-
-            else                // 위와 똑같은데, B전선을 까는 중이였다면 
-
-            {
-
-                B_Flag = true;
-
-                for (int i = 0; i < R.size(); i++)
-
-                {
-
-                    int Rx = R[i].first;
-
-                    int Ry = R[i].second;
-
-                    Visit[Rx][Ry] = 2;
-
- 
-
-                    B_Length++;
-
-                }
-
-            }
-
-            return;
-
+            return true;
         }
-
-    
-
-        for (int i = 0; i < 4; i++)
-
+        for (int i = 0; i < 4; ++i)
         {
+            nx = cx + dx[i];
+            ny = cy + dy[i];
 
-            int nx = x + dx[i];
+            if (OutOfBound(nx, ny) || is_visited[nx][ny].dist != -1)
+                continue;
 
-            int ny = y + dy[i];
+            if (!first_check && has_line[nx][ny])
+                continue;
 
-            vector<pair<int, int>> nR;
-
- 
-
-            for (int j = 0; j < R.size(); j++) nR.push_back(R[j]);
-
-            // 기존의 Route를 일단 다 옮겨준다.
-
-            
-
-            if (nx < 0 || ny < 0 || nx > N || ny > M) continue;
-
-            // 범위Out이면 continue
-
-            if (c == 'A')    
-
-            {
-
-                if ((nx == B_Pos[0].first && ny == B_Pos[0].second) || (nx == B_Pos[1].first && ny == B_Pos[1].second)) continue;
-
-                if (Temp_Visit[nx][ny] == true || Visit[nx][ny] != 0) continue;
-
-                // A를 까는 중일 때, B의 2점 중 한 점이 있는 곳이라면 그곳에는 전선을 깔 수 없다.
-
-                // A를 까는 중일 때, 이미 방문했거나, 이미 B의 전선이 깔려있는 곳이라면 전선을 깔 수 없다.
-
- 
-
-                Temp_Visit[nx][ny] = true;
-
-                nR.push_back(make_pair(nx, ny));
-
-                Q.push({ nx, ny, nR });
-
-            }
-
-            else // 위와 마찬가지인데 B전선을 까는 경우임. 즉, 비교해야될 조건만 A로 바뀌게 되는 것 ! 
-
-            {
-
-                if ((nx == A_Pos[0].first && ny == A_Pos[0].second) || (nx == A_Pos[1].first && ny == A_Pos[1].second)) continue;
-
-                if (Temp_Visit[nx][ny] == true || Visit[nx][ny] != 0) continue;
-
- 
-
-                Temp_Visit[nx][ny] = true;
-
-                nR.push_back(make_pair(nx, ny));
-
-                Q.push({ nx, ny, nR });
-
-            }
-
-        }        
-
-    }    
-
+            q.push({ nx,ny });
+            is_visited[nx][ny] = { cx,cy,length + 1 };
+        }
+    }
+    return false;
 }
 
- 
+void Init(bool run_bfs_a)
+{
+    fill_n(&is_visited[0][0], 101 * 101, track{ -1,-1,-1 });
+    if (run_bfs_a)
+    {
+        is_visited[points[2].first][points[2].second] = { -1,-1,0 };
+        is_visited[points[3].first][points[3].second] = { -1,-1,0 };
+    }
+    else
+    {
+        is_visited[points[0].first][points[0].second] = { -1,-1,0 };
+        is_visited[points[1].first][points[1].second] = { -1,-1,0 };
+    }
+}
 
 void Solution()
-
 {
+    int total_length = 0;
+    int length_1 = 0;
+    int length_2 = 0;
+    bool succeed_ab = true;
 
-    int x = A_Pos[0].first;
-
-    int y = A_Pos[0].second;
-
-    int xx = A_Pos[1].first;
-
-    int yy = A_Pos[1].second;
-
-    BFS(x, y, xx, yy, 'A');
-
- 
-
-    memset(Temp_Visit, false, sizeof(Temp_Visit));
-
-    x = B_Pos[0].first;
-
-    y = B_Pos[0].second;
-
-    xx = B_Pos[1].first;
-
-    yy = B_Pos[1].second;
-
-    BFS(x, y, xx, yy, 'B');
-
+    //a -> b
+    Init(true);
+    if (bfs(points[0], points[1], true))
+    {
+        length_1 = is_visited[points[1].first][points[1].second].dist;
+        Init(false);
+        if (bfs(points[2], points[3], false))
+        {
+            length_2 = is_visited[points[3].first][points[3].second].dist;
+            total_length = length_1 + length_2;
+        }
+        else
+            succeed_ab = false;
+    }
     
-
-    if (A_Flag == true && B_Flag == true)
-
+    //b -> a
+    Init(false);
+    fill_n(&has_line[0][0], 101 * 101, false);
+    if (bfs(points[2], points[3], true))
     {
-
-        Answer_Flag = true;
-
-        Answer = Min(Answer, A_Length + B_Length);
-
+        length_1 = is_visited[points[3].first][points[3].second].dist;
+        Init(true);
+        if (bfs(points[0], points[1], false))
+        {
+            length_2 = is_visited[points[1].first][points[1].second].dist;
+            if (succeed_ab)
+            {
+                total_length = min(total_length, length_1 + length_2);
+            }
+            else
+                total_length = length_1 + length_2;
+        }
     }
 
- 
-
-    memset(Visit, 0, sizeof(Visit));
-
-    memset(Temp_Visit, false, sizeof(Temp_Visit));
-
-    A_Length = B_Length = 0;
-
-    A_Flag = false;
-
-    B_Flag = false;
-
- 
-
-    x = B_Pos[0].first;
-
-    y = B_Pos[0].second;
-
-    xx = B_Pos[1].first;
-
-    yy = B_Pos[1].second;
-
-    BFS(x, y, xx, yy, 'B');
-
- 
-
-    memset(Temp_Visit, false, sizeof(Temp_Visit));
-
-    x = A_Pos[0].first;
-
-    y = A_Pos[0].second;
-
-    xx = A_Pos[1].first;
-
-    yy = A_Pos[1].second;
-
-    BFS(x, y, xx, yy, 'A');
-
- 
-
-    if (A_Flag == true && B_Flag == true)
-
-    {
-
-        Answer_Flag = true;
-
-        Answer = Min(Answer, A_Length + B_Length);
-
-    }
-
+    if (total_length == 0)
+        cout << "IMPOSSIBLE";
+    else
+        cout << total_length;
 }
-
- 
-
-void Solve()
-
+int main()
 {
-
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    
     Input();
-
     Solution();
 
- 
-
-    if (Answer_Flag == false) cout << "IMPOSSIBLE" << endl;
-
-    else cout << Answer - 2 << endl;
-
-}
-
- 
-
-int main(void)
-
-{
-
-    ios::sync_with_stdio(false);
-
-    cin.tie(NULL);
-
-    cout.tie(NULL);
-
- 
-
-    //freopen("Input.txt", "r", stdin);
-
-    Solve();
-
- 
-
     return 0;
-
 }
