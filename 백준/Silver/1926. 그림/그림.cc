33a6669cfb161e908c4d076628bdd38494c4e08f
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2026-01-23 00:12:40 +0900

    [Silver I] Title: 그림, Time: 24 ms, Memory: 2512 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,93 +1,99 @@
 #include <iostream>
+#include <algorithm>
 #include <queue>
+#include <tuple>
+#include <stack>
 
 using namespace std;
 
-int N, M;
-int painting[501][501];
-int is_visited[501][501];
-int cnt_paintings = 0;
-int max_area = 0;
+int n, m;
+bool board[501][501];
+bool is_visited[501][501];
 
-int dx[4] = {0,0,1,-1};
-int dy[4] = {1,-1,0,0};
+int dr[4] = { 0,0,1,-1 };
+int dc[4] = { 1,-1,0,0 };
 
-bool IsValid(int x, int y)
+void Input()
 {
-    if(x < 0 || y < 0 || x>= N || y >= M)
+    cin >> n >> m;
+    int value;
+    for (int i = 1; i <= n; ++i)
     {
-        return false;
-    }
-    
-    return true;
-}
-
-void bfs(int X, int Y)
-{
-    if(painting[X][Y] == 0)
-        return;
-    
-    if(is_visited[X][Y] > 0)
-        return; 
-    
-    cnt_paintings++;
-    is_visited[X][Y] = cnt_paintings;
-    int curr_area = 1;
-    
-    queue<pair<int,int>> q;
-    q.push({X, Y});
-    
-    while(!q.empty())
-    {
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-        
-        for(int i = 0; i < 4; ++i)
+        for (int j = 1; j <= m; ++j)
         {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
-            if(!IsValid(nx, ny) || is_visited[nx][ny] > 0)
-                continue;
-            
-            if(painting[nx][ny] == 1)
-            {
-                is_visited[nx][ny] = cnt_paintings;
-                q.push({nx, ny});
-                curr_area++;
-            }
+            cin >> value;
+            if (value == 0)
+                board[i][j] = false;
+            else
+                board[i][j] = true;
         }
     }
-    
-    max_area = max(max_area, curr_area);
+}
+
+bool OutOfBound(int r, int c)
+{
+    if (r <= 0 || c <= 0 || r > n || c > m)
+        return true;
+    return false;
+}
+
+int Count_Area(int r, int c)
+{
+    queue<pair<int, int>> q;
+    q.push({ r,c });
+    is_visited[r][c] = true;
+
+    int nr, nc;
+    int cr, cc;
+    int area = 0;
+
+    while (!q.empty())
+    {
+        tie(cr, cc) = q.front();
+        q.pop();
+        ++area;
+
+        for (int i = 0; i < 4; ++i)
+        {
+            nr = cr + dr[i];
+            nc = cc + dc[i];
+            if (OutOfBound(nr, nc))
+                continue;
+
+            if (is_visited[nr][nc] || !board[nr][nc])
+                continue;
+
+            q.push({ nr,nc });
+            is_visited[nr][nc] = true;
+
+        }
+    }
+
+    return area;
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    
-    cin >> N >> M;
-    
-    for(int i = 0; i< N; ++i)
+    cin.tie(0); cout.tie(0);
+
+    Input();
+
+    int max_area = 0;
+    int count_area = 0;
+
+    for (int i = 1; i <= n; ++i)
     {
-        for(int j = 0; j< M; ++j)
+        for (int j = 1; j <= m; ++j)
         {
-            cin >> painting[i][j];
+            if (board[i][j] && is_visited[i][j] == false)
+            {
+                ++count_area;
+                max_area = max(max_area, Count_Area(i, j));
+            }
         }
     }
-    
-    for(int i = 0; i< N; ++i)
-    {
-        for(int j = 0; j< M; ++j)
-        {
-            bfs(i, j);
-        }
-    }
-    
-    cout << cnt_paintings <<"\n" << max_area;
-    
+
+    cout << count_area << "\n" << max_area;
     return 0;
 }
