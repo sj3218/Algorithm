419c74fb191a0127092782d7184daff469ae97ee
Sejeong Park <74656802+sj3218@users.noreply.github.com>
2026-02-19 22:54:29 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -4,72 +4,80 @@
 
 using namespace std;
 
-int N, K;
-int dp[100001];
-const int INF = 0XFFFFFFF;
+const int BOARD_MAX = 100000;
+int N;
+int K;
+int is_visited[100001];
 
-bool CanDo(int value)
+void Input()
 {
-    if (value < 0 || value > 100000)
-        return false;
-    return true;
+	cin >> N >> K;
 }
-void bfs()
+
+bool OutOfRange(int x)
 {
-    fill_n(dp, 100001, INF);
-    queue<int> q;
-    q.push(N);
-    dp[N] = 0;
-    
-    int curr_pos;
-    int curr_time;
-    int value[3] = { 0,0,0 };
-    while (!q.empty())
-    {
-        curr_pos = q.front(); q.pop();
-        if (curr_pos == K)
-        {
-            cout << dp[curr_pos];
-            return;
-        }
-        curr_time = dp[curr_pos];
+	if (x < 0 || x > BOARD_MAX)
+		return true;
 
-        value[0] = curr_pos - 1;
-        value[1] = curr_pos + 1;
-        value[2] = curr_pos * 2;
-
-        for (int i = 0; i < 3; ++i)
-        {
-            if (!CanDo(value[i]))
-            {
-                continue;
-            }
-
-            if (i != 2)
-            {
-                if (dp[value[i]] != INF && dp[value[i]] <= curr_time+1)
-                    continue;
-                dp[value[i]] = curr_time+1;
-            }
-            else
-            {
-                if (dp[value[i]] != INF && dp[value[i]] <= curr_time)
-                    continue;
-                dp[value[i]] = curr_time;
-            }
-
-            q.push(value[i]);
-        }
-    }
-
+	return false;
 }
+
+void FindFastTime()
+{
+	fill_n(is_visited, 100001, -1);
+	queue<int> q;
+	is_visited[N] = 0;
+	q.push(N);
+
+	int curr_pos;
+	int curr_time;
+	int next_time;
+	int next_pos;
+	while (!q.empty())
+	{
+		curr_pos = q.front();
+		q.pop();
+		curr_time = is_visited[curr_pos];
+		if (curr_pos == K)
+			return;
+
+		for (int n_pos : {curr_pos + 1, curr_pos - 1})
+		{
+			if (OutOfRange(n_pos))
+				continue;
+
+			next_time = is_visited[n_pos];
+
+			if (next_time != -1 && next_time <= curr_time + 1)
+			{
+				continue;
+			}
+
+			is_visited[n_pos] = curr_time + 1;
+			q.push(n_pos);
+		}
+
+		next_pos = curr_pos * 2;
+		if (OutOfRange(next_pos))
+			continue;
+
+		next_time = is_visited[next_pos];
+		if (next_time != -1 && next_time <= curr_time)
+			continue;
+
+		is_visited[next_pos] = curr_time;
+		q.push(next_pos);
+	}
+}
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    
-    cin >> N >> K;
-    bfs();
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
 
-    return 0;
+	Input();
+	FindFastTime();
+	cout << is_visited[K];
+
+	return 0;
 }
